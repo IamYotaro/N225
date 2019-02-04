bb3849464c73132ec34a7dfb0c4a14ef95ceec10
IamYotaro <40481484+IamYotaro@users.noreply.github.com>
2019-02-04 23:21:10 +0900

    a

--- N225.py
+++ N225.py
@@ -22,11 +22,32 @@ N225 = web.DataReader("NIKKEI225","fred","1950/5/16")
 N225.index = pd.to_datetime(N225.index)
 N225.dropna(how='any', inplace=True)
 
+N225.to_csv('N225.csv')
+
+#%%
+N225 = pd.read_csv('N225.csv')
+N225.set_index('DATE', inplace=True)
+N225.index = pd.to_datetime(N225.index)
+
 #%%
 fig, ax = plt.subplots(1,1)
 ax.plot(N225)
 plt.show
 
+#%%
+volatility_column = 'NIKKEI225'
+
+def volatility(data):
+    volatility = np.zeros(len(data)-1)
+    for i in range(len(data)-1):
+        volatility[i] = 100 * np.log(data[volatility_column].iloc[i+1] / data[volatility_column].iloc[i])
+    volatility = pd.Series(data=volatility, index=data.iloc[1:].index, name=volatility_column)
+    return volatility
+
+#%%
+N225 = volatility(N225)
+#%%
+N225 = pd.DataFrame(N225)
 #%%
 def zscore(training_data, data):
     training_data_mean = np.mean(training_data, axis=0)
@@ -37,26 +58,34 @@ def zscore(training_data, data):
 def original_scale(predicted_data, training_data_mean, training_data_std):
     original_scale_predicted_data = training_data_std * predicted_data + training_data_mean
     return original_scale_predicted_data
+    
+confidence_interval_99 = [-2.58, 2.58]
 
 #%%
 time_length = 24
-n_pred = 1
-train_column = 'NIKKEI225'
+pred_category = 5
 target_column = 'NIKKEI225'
 
 def make_dataset(data):
     
     inputs_data = []
     
-    for i in range(len(data)-time_length-n_pred+1):
+    for i in range(len(data)-time_length):
         temp_set = data[i:(i+time_length)].copy()
-        temp_set.loc[:, train_column] = temp_set[train_column] / temp_set[train_column].iloc[0] - 1
         inputs_data.append(temp_set)
     
-    inputs_target = np.zeros(shape=(len(data)-time_length-n_pred+1, n_pred))
-    for i in range(len(data)-time_length-n_pred+1):
-        for j in range(n_pred):
-            inputs_target[i, j] = data[target_column][time_length + i + j]
+    inputs_target = np.zeros(shape=(len(data)-time_length, pred_category))
+    for i in range(len(data)-time_length):
+        if data[target_column][time_length + i] < confidence_interval_99[0]:
+            inputs_target[i, 0] = 1
+        elif confidence_interval_99[0] <= data[target_column][time_length + i] and data[target_column][time_length + i] < 0:
+            inputs_target[i, 1] = 1
+        elif data[target_column][time_length + i] == 0:
+            inputs_target[i, 2] = 1
+        elif 0 < data[target_column][time_length + i] and data[target_column][time_length + i] <= confidence_interval_99[1]:
+            inputs_target[i, 3] = 1
+        elif confidence_interval_99[0] < data[target_column][time_length + i]:
+            inputs_target[i, 4] = 1
 
     inputs_data_np = [np.array(inputs_data) for inputs_data in inputs_data]
     inputs_data_np = np.array(inputs_data_np)
@@ -68,7 +97,7 @@ def make_dataset(data):
 #%%
 # make Training data
 
-N225_train = N225.loc['1950-05-16 00:00:00':'2017-12-29 00:00:00', :]
+N225_train = N225.loc['1950-05-17 00:00:00':'2017-12-29 00:00:00', :]
 
 N225_train_normalized, N225_train_mean, N225_train_std = zscore(N225_train, N225_train)
 
@@ -77,7 +106,7 @@ LSTM_inputs_data_train, LSTM_inputs_target_train = make_dataset(N225_train_norma
 #%%
 # make Test data
 
-N225_test = N225.loc['2018-01-04 00:00:00':'2019-01-15 00:00:00']
+N225_test = N225.loc['2018-01-04 00:00:00':'2019-02-01 00:00:00']
 
 N225_test_normalized, N225_test_mean, N225_test_std = zscore(N225_train, N225_test)
 
@@ -96,7 +125,8 @@ LSTM_inputs_test_data = np.array(N225_test_normalized).reshape(1, time_length, l
 #%%
 from keras.models import Sequential
 from keras.layers.core import Dense
-from keras.layers import CuDNNLSTM
+#from keras.layers import CuDNNLSTM
+from keras.layers.recurrent import LSTM
 from keras import optimizers
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.utils import plot_model
@@ -106,16 +136,16 @@ np.random.seed(123)
 
 #%%
 in_dim = LSTM_inputs_data_train.shape[2]
-hidden_size = 858
-out_dim = n_pred
+hidden_size = 64
+out_dim = pred_category
 
 model = Sequential()
-model.add(CuDNNLSTM(hidden_size, return_sequences=False,
+model.add(LSTM(hidden_size, return_sequences=False,
                batch_input_shape=(None, time_length, in_dim)))
 model.add(Dense(out_dim, activation='linear'))
 
 adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
 model.summary()
 plot_model(model, to_file='model.png', show_shapes=True)
@@ -133,7 +163,7 @@ while True:
         model_checkpoint = ModelCheckpoint(filepath='best_model_checkpint.h5', monitor='val_loss', save_best_only=True)
         LSTM_history = model.fit(LSTM_inputs_data_train, LSTM_inputs_target_train,
                                  batch_size=128,
-                                 epochs=100,
+                                 epochs=30,
                                  validation_split=0.1,
                                  shuffle=False,
                                  callbacks=[model_checkpoint])
@@ -186,6 +216,7 @@ def MAPE(data, pred_data):
 #%%
 model.load_weights('best_model_checkpint.h5')
 predicted_N225_test = model.predict(LSTM_inputs_data_test)
+#%%
 predicted_N225_test_original_scale = original_scale(predicted_N225_test, N225_train_mean['NIKKEI225'], N225_train_std['NIKKEI225'])
 
 #%%
